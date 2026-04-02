f1c78b2adfc7f12fb4076ba5288f775f8f12be70
Prithivi <prithivi@users.noreply.github.com>
2026-04-02 13:14:32 +0545

    feat: add environment configuration file for server settings

---
+++ server/src/config/env.config.ts
@@ -0,0 +1,11 @@
+
+const ENV = {
+    PORT: process.env["PORT"],
+    NODE_ENV: process.env["NODE_ENV"],
+    MONGO_URI: process.env["MONGO_URI"],
+    JWT_SECRET: process.env["JWT_SECRET"],
+    JWT_EXPIRES_IN: process.env["JWT_EXPIRES_IN"],
+    JWT_COOKIE_EXPIRES_IN: process.env["JWT_COOKIE_EXPIRES_IN"],
+};
+
+export default ENV;
